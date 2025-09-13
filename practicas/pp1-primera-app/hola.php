9cb941de5522f6ade49d45e3fae162dc75511998
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2025-09-13 01:47:05 +0000

    CanoJoel_Pp1

---
+++ practicas/pp1-primera-app/hola.php
@@ -0,0 +1,44 @@
+
+<?php
+
+/*----------------------------------------------------------------------------------------
+ * BUENAS TARDES
+ *---------------------------------------------------------------------------------------*/
+
+function sayHello($name) {
+	echo "Hello $name!";
+}
+
+?>
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Pp1-CanoJoel</title>
+</head>
+
+<body>
+    <header>
+        <img src="../imagen/logo-fpllefia.jpg" alt="FPLlefia">
+        <h1>Módulo 7 – Práctica 1. Mi primera aplicación en PHP</h1>
+    </header>
+
+    <main>
+        <img src="fotomia" alt="Joel Cano">
+        <h2>Joel Cano</h2>
+    </main>
+    
+    <footer>
+        <p>Mi nombre es Joel Cano</p>
+        
+    </footer>
+
+    
+         
+        
+</body>
+
+</html>
+
+
