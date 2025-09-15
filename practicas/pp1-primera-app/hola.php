6b8c94d8107180c067f7317b1198dcf99dd8f478
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2025-09-15 15:09:26 +0000

    CanoJoel_Pp1

--- practicas/pp1-primera-app/hola.php
+++ practicas/pp1-primera-app/hola.php
@@ -1,37 +1,31 @@
 
-<?php
-
-/*----------------------------------------------------------------------------------------
- * BUENAS TARDES
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
-
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Pp1-CanoJoel</title>
+    <title><?= "Pp1-CanoJoel"; ?></title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
     <header>
-        <img src="../imagen/logo-fpllefia.jpg" alt="FPLlefia">
-        <h1>Módulo 7 – Práctica 1. Mi primera aplicación en PHP</h1>
+        <img id="logo" src="imagen/logo-fpllefia.png" alt="FPLlefia">
+        <h1><?= "Módulo 7 – Práctica 1. Mi primera aplicación en PHP"; ?></h1>
     </header>
 
     <main>
-        <img src="fotomia" alt="Joel Cano">
-        <h2>Joel Cano</h2>
+        <div id="perfil">
+            <div id="columnaFoto">
+                <img id="miFoto" src="imagen/yo.jpg" alt="Joel Cano">
+                <h2 id="miNombre"><?= "Joel Cano"; ?></h2>
+            </div>
+            <p id="textoExplicativo"><?="Lo subrayado muestra cómo se llama a funciones de PHP dentro del HTML. Primero, se usa sayHello('remote world'); para mostrar el mensaje 'Hello remote world!' usando la función definida antes. Después, se llama a phpinfo();, que muestra información detallada sobre la configuración de PHP en el servidor. Ambas llamadas están dentro de etiquetas <?php ... ?>, lo que permite mezclar PHP y HTML en la misma página." ?> </p>
+        </div>
+        
     </main>
     
     <footer>
-        <p>Mi nombre es Joel Cano</p>
-        
+        <p>Mi nombre es <?="Joel Cano"; ?></p>
+        <p>La fecha de hoy es <?= date("d/m/Y"); ?></p>
     </footer>
 
     
